b31cc9cbcd053c597826b62999787fe82e0353e2
Tim Klever <tim.klever@webpt.com>
2015-06-09 23:52:57 -0700

    Moving Error Throwing

--- src/ArrayUtils/AbstractArrayMiddleware.php
+++ src/ArrayUtils/AbstractArrayMiddleware.php
@@ -7,19 +7,14 @@ use Webpt\Aquaduck\Middleware\AbstractMiddleware;
 
 abstract class AbstractArrayMiddleware extends AbstractMiddleware
 {
-    public function __invoke($subject, $next = null)
+    protected function execute($subject)
     {
         if (!is_array($subject)) {
             throw new InvalidArgumentException(
-                sprintf('First parameter must be an array; received "%s"', gettype($subject))
+                sprintf('AbstractArrayMiddleware::execute must be passed an array; received "%s"', gettype($subject))
             );
         }
 
-        return parent::__invoke($subject, $next);
-    }
-
-    protected function execute($subject)
-    {
         return $this->executeArray($subject);
     }
 
